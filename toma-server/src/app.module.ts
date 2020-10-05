bf3d09d11624b6f28de2a52d78a953baca675340
AnimeshKC <animesh@ualberta.ca>
2020-10-04 21:26:16 -0600

    app imports and validations

--- toma-server/src/app.module.ts
+++ toma-server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
