dfd39de977de9200647a5bcaa6806081ed5ce8d9
AnimeshKC <animesh@ualberta.ca>
2020-10-04 21:24:37 -0600

    tweak user size constraints

---
+++ toma-server/src/migrations/1601852699914-varcharsize.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class varcharsize1601852699914 implements MigrationInterface {
+    name = 'varcharsize1601852699914'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "email" character varying(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying(255) NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "password" text NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "email" character varying(128) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+    }
+
+}
