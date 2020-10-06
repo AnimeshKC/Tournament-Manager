ed05c61aea2a1f5670693926a27f38bc643a9269
AnimeshKC <animesh@ualberta.ca>
2020-10-06 10:58:12 -0600

    method for authenticating the user

--- toma-server/src/auth/auth.service.ts
+++ toma-server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import bcrypt from "bcrypt";
 import RegisterDto from "./dto/register.dto";
+import PostgresErrorCode from "src/database/postgresErrorCodes.enum";
 const saltRounds = 10;
 @Injectable()
 export class AuthService {
@@ -19,9 +20,35 @@ export class AuthService {
       createdUser.password = undefined;
       return createdUser;
     } catch (error) {
+      if (error?.code === PostgresErrorCode.UniqueViolation) {
+        const message = error?.detail || "Username or Email already exists"; //this fail-safe is likely unneeded
+        throw new HttpException(error.detail, HttpStatus.BAD_REQUEST);
+      }
+      const message: string = error?.message || "Something went wrong";
+      //TODO: More precise Error Handling
+      throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  public async getAuthenticatedUser(username: string, password: string) {
+    try {
+      const user = await this.usersService.findByUsername(username);
+      await this.verifyPassword(password, user.password);
+      user.password = undefined;
+      return user;
+    } catch (error) {
+      //TODO: Fine Tune Error Handling
       throw new HttpException(
-        "Something went wrong",
-        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Wrong credentials provided",
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+  private async verifyPassword(password: string, hashedPassword: string) {
+    const isPasswordMatching = await bcrypt.compare(password, hashedPassword);
+    if (!isPasswordMatching) {
+      throw new HttpException(
+        "Wrong credentials provided",
+        HttpStatus.BAD_REQUEST,
       );
     }
   }
---
+++ toma-server/src/database/postgresErrorCodes.enum.ts
@@ -0,0 +1,4 @@
+enum PostgresErrorCode {
+  UniqueViolation = "23505",
+}
+export default PostgresErrorCode;
