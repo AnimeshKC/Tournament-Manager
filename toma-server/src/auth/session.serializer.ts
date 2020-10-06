081131e82105289fd5410441542041ec50a94a5b
AnimeshKC <animesh@ualberta.ca>
2020-10-06 11:01:42 -0600

    session serialization and cookie parsing

---
+++ toma-server/src/auth/session.serializer.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { PassportSerializer } from "@nestjs/passport";
+import { User } from "src/entity/user.entity";
+
+@Injectable()
+export class SessionSerializer extends PassportSerializer {
+  serializeUser(user: User, done: (err: Error, id?: any) => void) {
+    return done(null, user.id);
+  }
+  deserializeUser(payload: any, done: (err: Error, value: any) => void) {
+    return done(null, payload);
+  }
+}
--- toma-server/src/main.ts
+++ toma-server/src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
+import cookieSession from "cookie-session";
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
+  require("dotenv").config();
+
 import { AppModule } from "./app.module";
 
+cookieParser;
+cookieSession;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(cookieSession({ secret: process.env.SESSION_SECRET }));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
