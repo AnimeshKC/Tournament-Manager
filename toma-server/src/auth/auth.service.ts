fab424da60b3aaee8b1ad7de32fbf904a7611808
AnimeshKC <animesh@ualberta.ca>
2020-10-07 07:25:30 -0600

    simplifying errors

--- toma-server/src/auth/auth.service.ts
+++ toma-server/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import bcrypt from "bcrypt";
 import RegisterDto from "./dto/register.dto";
@@ -17,6 +22,7 @@ export class AuthService {
         ...registrationData,
         password: hashedPassword,
       });
+      if (!createdUser) throw new UnauthorizedException("Unknown user");
       createdUser.password = undefined;
       return createdUser;
     } catch (error) {
@@ -24,24 +30,14 @@ export class AuthService {
         const message = error?.detail || "Username or Email already exists"; //this fail-safe is likely unneeded
         throw new HttpException(error.detail, HttpStatus.BAD_REQUEST);
       }
-      const message: string = error?.message || "Something went wrong";
-      //TODO: More precise Error Handling
-      throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw error;
     }
   }
   public async getAuthenticatedUser(username: string, password: string) {
-    try {
-      const user = await this.usersService.findByUsername(username);
-      await this.verifyPassword(password, user.password);
-      user.password = undefined;
-      return user;
-    } catch (error) {
-      //TODO: Fine Tune Error Handling
-      throw new HttpException(
-        "Wrong credentials provided",
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+    const user = await this.usersService.findByUsername(username);
+    await this.verifyPassword(password, user.password);
+    user.password = undefined;
+    return user;
   }
   private async verifyPassword(password: string, hashedPassword: string) {
     const isPasswordMatching = await bcrypt.compare(password, hashedPassword);
