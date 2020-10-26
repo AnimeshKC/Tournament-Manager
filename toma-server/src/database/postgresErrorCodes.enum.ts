28c01d72e415e72ba9475d61bdbe78774f0e7b0d
AnimeshKC <animesh@ualberta.ca>
2020-10-25 20:01:55 -0600

    adding integrity error codes

--- toma-server/src/database/postgresErrorCodes.enum.ts
+++ toma-server/src/database/postgresErrorCodes.enum.ts
@@ -1,4 +1,9 @@
 enum PostgresErrorCode {
   UniqueViolation = "23505",
+  CheckViolation = "23514",
+  ExclusionViolation = "23P01",
+  NonNullViolation = "23502",
+  RestrictViolation = "23001",
+  IntegrityConstraintViolation = "23000",
 }
 export default PostgresErrorCode;
