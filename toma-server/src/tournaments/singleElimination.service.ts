2342a32fafed970211d331971dfb9fd7ed439e44
AnimeshKC <animesh@ualberta.ca>
2020-11-05 08:39:50 -0700

    Cleanup Error Handling; support propagation

--- toma-server/src/tournaments/singleElimination.service.ts
+++ toma-server/src/tournaments/singleElimination.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import PostgresErrorCode from "src/database/postgresErrorCodes.enum";
 import { Repository } from "typeorm";
+import { Propagation, Transactional } from "typeorm-transactional-cls-hooked";
 import { SingleElimMember } from "./entities/singleElimMember.entity";
 
 @Injectable()
@@ -10,28 +11,22 @@ export class SingleEliminationService {
     @InjectRepository(SingleElimMember)
     private singleElimRepository: Repository<SingleElimMember>,
   ) {}
+  @Transactional({ propagation: Propagation.SUPPORTS })
   async addParticipant(participantData: {
     tournId: number;
     participantName?: string;
     userId?: number;
   }) {
-    try {
-      const entry = this.singleElimRepository.create(participantData);
-      await this.singleElimRepository.save(entry);
+    const entry = this.singleElimRepository.create(participantData);
+    await this.singleElimRepository.save(entry);
 
-      const { tournId, participantName, userId } = entry;
+    const { tournId, participantName, userId } = entry;
 
-      return {
-        tournId,
-        participantName,
-        userId,
-        type: "Single Elimination",
-      };
-    } catch (error) {
-      if (error?.code === PostgresErrorCode.UniqueViolation) {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      }
-      throw error;
-    }
+    return {
+      tournId,
+      participantName,
+      userId,
+      type: "Single Elimination",
+    };
   }
 }
