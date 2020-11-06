e0af6ddc4827e885d59a6474ab3df3121bf121d1
AnimeshKC <animesh@ualberta.ca>
2020-11-06 11:19:45 -0700

    set tournSize when tournament is initialized

--- toma-server/src/tournaments/singleElimination.service.ts
+++ toma-server/src/tournaments/singleElimination.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Propagation, Transactional } from "typeorm-transactional-cls-hooked";
 import { SingleElimDetails } from "./entities/singleElimDetails.entity";
 import { SingleElimMember } from "./entities/singleElimMember.entity";
+import { TournamentService } from "./tournaments.service";
+import { MemberVariants } from "./types/memberTables.enum";
 
 @Injectable()
 export class SingleEliminationService {
@@ -11,7 +13,8 @@ export class SingleEliminationService {
     @InjectRepository(SingleElimMember)
     private singleElimRepository: Repository<SingleElimMember>,
     @InjectRepository(SingleElimDetails)
-    private singleElimDetails: Repository<SingleElimDetails>,
+    private detailsRepository: Repository<SingleElimDetails>,
+    private tournamentService: TournamentService,
   ) {}
 
   @Transactional({ propagation: Propagation.SUPPORTS })
@@ -32,14 +35,33 @@ export class SingleEliminationService {
       type: "Single Elimination",
     };
   }
-  @Transactional({ propagation: Propagation.SUPPORTS })
-  async createDetails(tournId: number) {
-    const detail = this.singleElimDetails.create({ tournId });
-    await this.singleElimDetails.save(detail);
+  private getTournSize(memberSize: number) {
+    if (memberSize <= 1)
+      throw new HttpException(
+        "Tournament must have at least 2 members",
+        HttpStatus.BAD_REQUEST,
+      );
+    let tournSize = 2;
+    while (tournSize < memberSize) {
+      tournSize = tournSize * 2;
+    }
+    return tournSize;
   }
   @Transactional({ propagation: Propagation.SUPPORTS })
+  async createDetails(tournId: number) {
+    const detail = this.detailsRepository.create({ tournId });
+    await this.detailsRepository.save(detail);
+  }
+  @Transactional()
   async initialize(tournId: number) {
-    const details = await this.singleElimDetails.findOne({ tournId });
-    // details.tournSize = await this.singleElimRepository.
+    const [details, members] = await Promise.all([
+      this.detailsRepository.findOne({ tournId }),
+      this.tournamentService.getTournamentMembers({
+        relationString: MemberVariants.singleElim,
+        tournId,
+      }),
+    ]);
+    details.tournSize = this.getTournSize(members.length);
+    await this.detailsRepository.save(details);
   }
 }
