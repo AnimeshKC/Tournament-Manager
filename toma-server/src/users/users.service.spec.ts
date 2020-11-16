bbe30c533db506e4f65b952e6a354a10a4057b57
AnimeshKC <animesh@ualberta.ca>
2020-11-15 18:40:40 -0700

    Use inmemory-db for userRepo value

--- toma-server/src/users/users.service.spec.ts
+++ toma-server/src/users/users.service.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
+import { getInMemoryDB } from "../database/in-memory-db";
+import { User } from "./entities/user.entity";
+import { UsersService } from "./users.service";
 
-describe('UsersService', () => {
+describe("UsersService", () => {
   let service: UsersService;
-
+  let conn: Connection;
   beforeEach(async () => {
+    conn = await getInMemoryDB([User]);
+    const users = conn.getRepository(User);
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: users },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
+  afterEach(async () => {
+    await conn.close();
+  });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
