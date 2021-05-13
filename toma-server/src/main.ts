25dab8c150915e30b833924157e5bfb0770fe1b4
AnimeshKC <animesh@ualberta.ca>
2021-05-13 17:55:03 -0600

    Setup MVC

--- toma-server/src/main.ts
+++ toma-server/src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 import {
   initializeTransactionalContext,
   patchTypeORMRepositoryWithBaseRepository,
@@ -14,9 +16,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   initializeTransactionalContext();
   patchTypeORMRepositoryWithBaseRepository();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, "..", "public"));
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.setViewEngine("hbs");
   await app.listen(5000);
 }
 bootstrap();
