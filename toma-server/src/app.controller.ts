0ab267f439d20423240eaf4a5510fd266e0a8872
AnimeshKC <animesh@ualberta.ca>
2021-05-14 07:37:25 -0600

    Render login page

--- toma-server/src/app.controller.ts
+++ toma-server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/")
+  @Render("login")
+  getHello() {
+    return { message: this.appService.getHello() };
   }
 }
--- toma-server/src/main.ts
+++ toma-server/src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
+import { resolve } from "path";
 import {
   initializeTransactionalContext,
   patchTypeORMRepositoryWithBaseRepository,
 } from "typeorm-transactional-cls-hooked";
+import * as exphbs from "express-handlebars";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
   require("dotenv").config();
@@ -19,8 +21,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  app.useStaticAssets(join(__dirname, "..", "public"));
-  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.useStaticAssets(resolve("./src/public"));
+  app.setBaseViewsDir(resolve("./src/views"));
   app.setViewEngine("hbs");
   await app.listen(5000);
 }
